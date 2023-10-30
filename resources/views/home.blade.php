d7bb09cc1ad92b43eb3a0703e7420c6277564193
marcocentonze <marco.centonze27@gmail.com>
2023-10-30 17:38:25 +0100

    add style to the markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,45 +4,50 @@
 
 @section('content')
 
-<h1 class="text-center text-danger">TRENITALIA</h1>
+    <h1 class="text-center text-black">TRENITALIA</h1>
 
-<div class="container mt-5">
-    <div class="d-flex justify-content-between">
+    <div class="container mt-5">
+        <div class="d-flex justify-content-between">
 
-        <!-- treni in partenza -->
-        <div class="flex-fill mr-3 bg-light p-3 border">
-            <h4>Treni in Partenza</h4>
-            @foreach($treniInPartenza as $treno)
-                <div class="pb-4">
-                    <h6>Azienda:</h6> {{ $treno->azienda }}
-                    <h6>Stazione di Partenza:</h6> {{ $treno->stazione_partenza }}
-                    <h6>Orario di Partenza:</h6> {{ $treno->orario_partenza }}
-                    <h6>Codice Treno:</h6> {{ $treno->codice_treno }}
-                    <h6>Numero Carrozze:</h6> {{ $treno->numero_carrozze }}
-                    <h6>In Orario:</h6> {{ $treno->in_orario ? 'Sì' : 'No' }}
-                    <h6>Cancellato:</h6> {{ $treno->cancellato ? 'Sì' : 'No' }}
-                </div>
-            @endforeach
+            <!-- treni in partenza -->
+            <div class="flex-fill mr-3 bg-light p-3 border rounded shadow-sm bg-white mb-4">
+                <h4><i class="fas fa-train px-2"></i>Treni in Partenza</h4>
+                @foreach ($treniInPartenza as $treno)
+                    <div class="pb-4 border-bottom">
+                        <h6>Azienda:</h6> {{ $treno->azienda }}
+                        <h6>Stazione di Partenza:</h6> {{ $treno->stazione_partenza }}
+                        <h6>Orario di Partenza:</h6> {{ $treno->orario_partenza }}
+                        <h6>Codice Treno:</h6> {{ $treno->codice_treno }}
+                        <h6>Numero Carrozze:</h6> {{ $treno->numero_carrozze }}
+                        <h6>In Orario:</h6>
+                        {!! $treno->in_orario ? 'Si <i class="fa-solid fa-face-grin-stars" style="color: #d5f00a;"></i>' : 'No <i class="fa-solid fa-heart-crack" style="color: #eb0000;"></i>' !!}
+                        <h6>Cancellato:</h6>
+                        {!! $treno->cancellato ? 'No <i class="fa-solid fa-face-grin-stars" style="color: #d5f00a;"></i>' : 'Si <i class="fa-solid fa-heart-crack" style="color: #eb0000;"></i>' !!}
+                       
+                    </div>
+                @endforeach
+            </div>
+
+            <!-- treni in arrivo -->
+            <div class="flex-fill ml-3 bg-light p-3 border rounded shadow-sm bg-white mb-4">
+                <h4><i class="fas fa-train px-2"></i>Treni in Arrivo</h4>
+                @foreach ($treniInArrivo as $treno)
+                    <div class="pb-4 border-bottom">
+                        <h6>Azienda:</h6> {{ $treno->azienda }}
+                        <h6>Stazione di Arrivo:</h6> {{ $treno->stazione_arrivo }}
+                        <h6>Orario di Arrivo:</h6> {{ $treno->orario_arrivo }}
+                        <h6>Codice Treno:</h6> {{ $treno->codice_treno }}
+                        <h6>Numero Carrozze:</h6> {{ $treno->numero_carrozze }}
+                        <h6>In Orario:</h6>
+                        {!! $treno->in_orario ? 'Si <i class="fa-solid fa-face-grin-stars" style="color: #d5f00a;"></i>' : 'No <i class="fa-solid fa-heart-crack" style="color: #eb0000;"></i>' !!}
+                        <h6>Cancellato:</h6>
+                        {!! $treno->cancellato ? 'No <i class="fa-solid fa-face-grin-stars" style="color: #d5f00a;"></i>' : 'Si <i class="fa-solid fa-heart-crack" style="color: #eb0000;"></i>' !!}
+                       
+                @endforeach
+            </div>
+
         </div>
-
-        <!-- treni in arrivo -->
-        <div class="flex-fill ml-3 bg-light p-3 border">
-            <h4>Treni in Arrivo</h4>
-            @foreach($treniInArrivo as $treno)
-                <div class="pb-4">
-                    <h6>Azienda:</h6> {{ $treno->azienda }}
-                    <h6>Stazione di Arrivo:</h6> {{ $treno->stazione_arrivo }}
-                    <h6>Orario di Arrivo:</h6> {{ $treno->orario_arrivo }}
-                    <h6>Codice Treno:</h6> {{ $treno->codice_treno }}
-                    <h6>Numero Carrozze:</h6> {{ $treno->numero_carrozze }}
-                    <h6>In Orario:</h6> {{ $treno->in_orario ? 'Sì' : 'No' }}
-                    <h6>Cancellato:</h6> {{ $treno->cancellato ? 'Sì' : 'No' }}
-                </div>
-            @endforeach
-        </div>
-
     </div>
-</div>
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel @yield('title') </title>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -19,7 +19,7 @@
 
     @include('partials.header')
 
-    <main class="">
+    <main class="" style="background-color: rgb(211, 46, 46)">
         @yield('content')
     </main>
 
